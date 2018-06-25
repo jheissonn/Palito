d9436f32abd9a66d63e0fac6dfafe34a07027ea1
Jheisson <jh.eison@Hotmail.com>
2018-06-25 12:52:56 -0300

    Projeto falta reestart

---
+++ palito/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace palito
+{
+    partial class Formpalito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelquanpalito = new System.Windows.Forms.Label();
+            this.numericUpDownquantpali = new System.Windows.Forms.NumericUpDown();
+            this.buttonjogar = new System.Windows.Forms.Button();
+            this.labelretirada = new System.Windows.Forms.Label();
+            this.buttonretiradapali = new System.Windows.Forms.Button();
+            this.maskedTextBoxpalitos = new System.Windows.Forms.MaskedTextBox();
+            this.richTextBoxmostratudo = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownquantpali)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelquanpalito
+            // 
+            this.labelquanpalito.AutoSize = true;
+            this.labelquanpalito.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelquanpalito.Location = new System.Drawing.Point(13, 13);
+            this.labelquanpalito.Name = "labelquanpalito";
+            this.labelquanpalito.Size = new System.Drawing.Size(210, 16);
+            this.labelquanpalito.TabIndex = 0;
+            this.labelquanpalito.Text = "escolha a quantidade de palistos:";
+            // 
+            // numericUpDownquantpali
+            // 
+            this.numericUpDownquantpali.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownquantpali.Location = new System.Drawing.Point(272, 8);
+            this.numericUpDownquantpali.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDownquantpali.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDownquantpali.Name = "numericUpDownquantpali";
+            this.numericUpDownquantpali.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownquantpali.TabIndex = 1;
+            this.numericUpDownquantpali.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // buttonjogar
+            // 
+            this.buttonjogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonjogar.Location = new System.Drawing.Point(438, 8);
+            this.buttonjogar.Name = "buttonjogar";
+            this.buttonjogar.Size = new System.Drawing.Size(75, 23);
+            this.buttonjogar.TabIndex = 2;
+            this.buttonjogar.Text = "jogar";
+            this.buttonjogar.UseVisualStyleBackColor = true;
+            this.buttonjogar.Click += new System.EventHandler(this.buttonjogar_Click);
+            // 
+            // labelretirada
+            // 
+            this.labelretirada.AutoSize = true;
+            this.labelretirada.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelretirada.Location = new System.Drawing.Point(13, 53);
+            this.labelretirada.Name = "labelretirada";
+            this.labelretirada.Size = new System.Drawing.Size(238, 16);
+            this.labelretirada.TabIndex = 3;
+            this.labelretirada.Text = "escolha  quantos palitos deseja retirar:";
+            // 
+            // buttonretiradapali
+            // 
+            this.buttonretiradapali.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonretiradapali.Location = new System.Drawing.Point(438, 46);
+            this.buttonretiradapali.Name = "buttonretiradapali";
+            this.buttonretiradapali.Size = new System.Drawing.Size(75, 23);
+            this.buttonretiradapali.TabIndex = 4;
+            this.buttonretiradapali.Text = "retirar";
+            this.buttonretiradapali.UseVisualStyleBackColor = true;
+            this.buttonretiradapali.Click += new System.EventHandler(this.buttonretiradapali_Click);
+            // 
+            // maskedTextBoxpalitos
+            // 
+            this.maskedTextBoxpalitos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBoxpalitos.Location = new System.Drawing.Point(272, 48);
+            this.maskedTextBoxpalitos.Mask = "0";
+            this.maskedTextBoxpalitos.Name = "maskedTextBoxpalitos";
+            this.maskedTextBoxpalitos.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBoxpalitos.TabIndex = 5;
+            // 
+            // richTextBoxmostratudo
+            // 
+            this.richTextBoxmostratudo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxmostratudo.Location = new System.Drawing.Point(-1, 73);
+            this.richTextBoxmostratudo.Name = "richTextBoxmostratudo";
+            this.richTextBoxmostratudo.Size = new System.Drawing.Size(931, 375);
+            this.richTextBoxmostratudo.TabIndex = 6;
+            this.richTextBoxmostratudo.Text = "";
+            // 
+            // Formpalito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.richTextBoxmostratudo);
+            this.Controls.Add(this.maskedTextBoxpalitos);
+            this.Controls.Add(this.buttonretiradapali);
+            this.Controls.Add(this.labelretirada);
+            this.Controls.Add(this.buttonjogar);
+            this.Controls.Add(this.numericUpDownquantpali);
+            this.Controls.Add(this.labelquanpalito);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Formpalito";
+            this.Text = "Jogo do Palito!!!";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownquantpali)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelquanpalito;
+        private System.Windows.Forms.NumericUpDown numericUpDownquantpali;
+        private System.Windows.Forms.Button buttonjogar;
+        private System.Windows.Forms.Label labelretirada;
+        private System.Windows.Forms.Button buttonretiradapali;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxpalitos;
+        private System.Windows.Forms.RichTextBox richTextBoxmostratudo;
+    }
+}
+
---
+++ palito/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace palito
+{
+    public partial class Formpalito : Form
+    { private int quantpali, resto, palitos ,bandeira=0;
+        public Formpalito()
+        {
+            InitializeComponent();
+            maskedTextBoxpalitos.Visible = false;//inicia invisel 
+            labelretirada.Visible = false;//inicia invisel 
+            buttonretiradapali.Visible = false;//inicia invisel 
+        }
+
+        private void buttonretiradapali_Click(object sender, EventArgs e)
+        {
+            
+            if (maskedTextBoxpalitos.Text == "")//para mostrar mensagem caso o usuário não digite nenhum numero
+            {
+                MessageBox.Show("voce deve preencher com numero de palitos");
+            }
+            else
+            {
+                palitos = Convert.ToInt16(maskedTextBoxpalitos.Text);
+                if (palitos == 1 || palitos == 2 || palitos == 3)//caso seja diferente dos 3 numeros ele deve emitir a mensagem que está no else
+                {
+                    quantpali = quantpali - palitos;
+                    if (palitos == 1)//serve para indentificar se ele perdeu ou não 
+                    {
+                        if (quantpali <= 0)//pra saber se o jogo ainda nao acabou
+                        {
+                            MessageBox.Show("voce perdeu","EASY");
+                            bandeira = 2;//para entrar no if de fim de jogo que coloquei lá em baixo
+                        }
+
+                    }
+                    if (palitos == 2)//para saber se ele não está tirando mais do que pode!!ex:só tem 1 palito e ele tirar 2
+                    {
+                        if (quantpali < 1)//caso seja maior ele emitira a mensagem logo a baixo
+                        {
+                            MessageBox.Show("voce não pode retirar esta quantidade de palitos","ATENÇÃO");
+                            bandeira = 1;//para ele não retirar os palitos
+                        }
+                    }
+                    if (palitos == 3)//para saber se ele não está tirando mais do que pode!!ex:só tem 1 palito e ele tirar 3
+                    {
+                        if (quantpali < 1)//caso seja maior ele emitira a mensagem logo a baixo
+                        {
+                            MessageBox.Show("voce não pode retirar esta quantidade de palitos", "ATENÇÃO");
+                            bandeira = 1;//para ele não retirar os palitos
+                        }
+                    }
+                    if (bandeira == 0)//serve para evitar o usuário não digitar nada diferente do que deveria!ex:só tem 1 palito e ele tirar 3
+                    {
+                        richTextBoxmostratudo.AppendText("eu retiro " + palitos.ToString() + " palitos (jogador)");
+                        richTextBoxmostratudo.AppendText(Environment.NewLine);
+                        richTextBoxmostratudo.AppendText("sobraram " + quantpali.ToString() + " palitos (jogador)");
+                        richTextBoxmostratudo.AppendText(Environment.NewLine);
+                    }
+                    resto = quantpali % 4;//saber o resto 
+                    if (bandeira == 0)//não permite retirar,caso o usuário tente retirar mais palitos do que o possivel 
+                    {
+                        if (resto == 0)//baseado no resto para sobrar sempre 1 no final retiro um valor pré estipulado
+                        {
+                            palitos = 3;
+                        }
+                        if (resto == 1)
+                        {
+                            palitos = 3;
+                        }
+                        if (resto == 2)
+                        {
+                            palitos = 1;
+                        }
+                        if (resto == 3)
+                        {
+                            palitos = 2;
+                        }
+                        quantpali = quantpali - palitos;//retira os palitos de acordo com o resto e mostra logo em seguida
+                        if (quantpali >= 1)
+                        {
+                            richTextBoxmostratudo.AppendText("eu retiro " + palitos.ToString() + " palitos (pc)");
+                            richTextBoxmostratudo.AppendText(Environment.NewLine);
+                            richTextBoxmostratudo.AppendText("sobraram " + quantpali.ToString() + " palitos (pc)");
+                            richTextBoxmostratudo.AppendText(Environment.NewLine);
+                        }
+                        else
+                        {
+                            MessageBox.Show("eu perdi(PC)", "IMPOSSIVEL");
+                            bandeira = 2;//para entrar no if de fim de jogo que coloquei lá em baixo
+                        }
+                    }
+                }
+                else//mensagem caso seja diferente de 1,2,3 palitos
+                {
+                    MessageBox.Show("são permtidos retirar de 1 a 3 palitos");
+                }
+            }
+            if (bandeira == 2)
+            {           
+                MessageBoxButtons botao = MessageBoxButtons.YesNo;
+                DialogResult resultado = MessageBox.Show("voce deseja jogar novamente?", "fim de jogo", botao);
+                if (resultado == DialogResult.Yes)
+                {
+                    maskedTextBoxpalitos.Visible = false;
+                    labelretirada.Visible = false;
+                    buttonretiradapali.Visible = false;
+                    buttonjogar.Enabled = true;
+                    numericUpDownquantpali.Enabled = true;
+                    richTextBoxmostratudo.Text = "";
+                    bandeira = 0;//para ele não entrar no mesmo if caso eu reinicie o jogo
+                }
+                else
+                {                 
+                    this.Close();//fecha a aplicação
+                }
+            }
+        }
+        private void buttonjogar_Click(object sender, EventArgs e)
+        {
+            buttonjogar.Enabled = false;//desliga para não poder clicar
+            numericUpDownquantpali.Enabled = false;//desliga para não poder clicar
+            maskedTextBoxpalitos.Visible = true;//torna visivel para poder jogar
+            labelretirada.Visible = true;
+            buttonretiradapali.Visible = true;
+            quantpali = (int)numericUpDownquantpali.Value;
+            resto = quantpali % 4;
+            if (bandeira != 1)
+            {
+                if (resto == 0)
+                {
+                    palitos = 3;
+                }
+                if (resto == 1)
+                {
+                    palitos = 3;
+                }
+                if (resto == 2)
+                {
+                    palitos = 1;
+                }
+                if (resto == 3)
+                {
+                    palitos = 2;
+                }
+                quantpali = quantpali - palitos;
+                richTextBoxmostratudo.AppendText("eu começo (pc)");
+                richTextBoxmostratudo.AppendText(Environment.NewLine);
+                richTextBoxmostratudo.AppendText("eu retiro " + palitos.ToString() + " palitos (pc)");
+                richTextBoxmostratudo.AppendText(Environment.NewLine);
+                richTextBoxmostratudo.AppendText("sobraram " + quantpali.ToString() + " palitos (pc)");
+                richTextBoxmostratudo.AppendText(Environment.NewLine);
+            }
+        }
+    }
+}
